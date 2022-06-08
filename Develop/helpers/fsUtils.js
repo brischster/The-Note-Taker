958a81a6b5a86ea2e624168d814ffdd97288cfdd
Ryan Brisch <brischster@gmail.com>
2022-06-07 20:53:43 -0600

    route is working for get and posting to the db file but need to get to render new to page

---
+++ Develop/helpers/fsUtils.js
@@ -0,0 +1,14 @@
+// const fs = require("fs");
+// const util = require("util");
+
+// const readFromFile = util.promisify(fs.readFile);
+
+// /**
+//  * @param {string} destination
+//  * @param {object} content
+//  * @returns {void}
+//  */
+// const writeToFile = (destination, content) =>
+//   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+//     err ? console.error(err) : console.info(`\nData written to ${destination}`)
+//   );
